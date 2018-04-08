63239d94845a97ef801694276eb0c07c40e55f49
Phillip Mates <phillipmates@gmail.com>
2018-04-08 23:21:01 +0200

    WIP: sketch postman

--- test/the_next_big_server_side_thing/customer_flow_test.clj
+++ test/the_next_big_server_side_thing/customer_flow_test.clj
@@ -5,18 +5,52 @@
             [the-next-big-server-side-thing.components.mock-http :as mock-http]
             [the-next-big-server-side-thing.http-helpers :refer [GET POST]]))
 
-(defn get-customer! [world]
+(defn get-customer! [id world]
   (mock-http/with-responses
     ["http://customer-service.com/by-name/bob" {:body {:name "bob"}}]
     (assoc world
            :customer-http-resp
            (GET "/customer" 200))))
 
-(flow "check service version"
+(defn create-account! [customer-id world]
+  (assoc world :created-account nil))
+
+(defn account-balance [account-id world]
+  (assoc world :account-balance 0))
+
+(defn delete-account! [account-id world]
+  world)
+
+(defn get-account-from-customer-id [customer-id world]
+  (assoc world :account nil))
+
+(def bob-id 1)
+
+(flow "create savings account and check balance"
   aux.init/init!
   aux.init/restart!
 
-  get-customer!
+  (partial get-account-from-customer-id bob-id)
 
-  (fact "we can get user links"
-    (:customer-http-resp *world*) => nil))
+  (fact "There shouldn't be a savings account for bob yet"
+    (:account *world*) => nil)
+
+  create-account!
+
+  (partial get-account-from-customer-id bob-id)
+
+  (fact "There should now be a savings account for bob"
+    (:account *world*) => nil)
+
+
+  (partial delete-account! bob-id)
+
+  (partial get-account-from-customer-id bob-id)
+
+  (fact "After deleting the savings account it can no longer be found"
+    (:account *world*) => nil)
+  )
+
+;; logging at each step
+;; retry logic baked in
+;; offers some measure of uniformity and re-use
